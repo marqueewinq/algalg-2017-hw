f3f485e1c85cecc1c2e985d95fcd710c5a4caa15
marqueewinq <marqueewinq@gmail.com>
2017-11-03 15:37:37 +0300

    refactored task-1

--- task-1/main.py
+++ task-1/main.py
@@ -1,18 +1,27 @@
-from pprint import pprint
-
 
 class PolarBear:
+    """
+        Solution for https://stepik.org/lesson/59290/step/1
+
+        Args:
+            n : `int` basis size for poly
+    """
+
+    #
+    # Label types
+    #
     INPUT, NOT, OR, AND = range(4) # label types
     verbose = [None, "NOT", "OR", "AND",] # verbose label types for printing
-    executive = [lambda x: None, lambda x: int(not x[0]), lambda x: int(any(x)), lambda x: int(all(x)),]
+    executive = [lambda x: None,
+                 lambda x: int(not x[0]),
+                 lambda x: int(any(x)),
+                 lambda x: int(all(x)),] # related functions
 
-    collected_function_set = []
-    collected_function_vtx = []
-    graph_evaluated = True
-    
     def __init__(self, n):
         self.n = n
         self.reversed_linklist = {} # graph repr
+        self.collected_function_set = []
+        self.graph_evaluated = True
 
         # adding inputs
         for i in range(self.n):
@@ -21,6 +30,9 @@ class PolarBear:
 
 
     def __str__(self):
+        """
+            Returns graph's representation as required in task.
+        """
         s = ''
         # printing edges
         for key, (label_ty, links) in self.reversed_linklist.items():
@@ -37,28 +49,103 @@ class PolarBear:
         return len(self.reversed_linklist)
 
 
-    def inputs(self, graph = None):
-        if graph is None:
-            graph = self.reversed_linklist
-        return [key for key, (label_ty, _) in graph.items() if label_ty == self.INPUT]
+    def add_vertex(self, label_ty, v_from):
+        """
+            Adds a new functional vertex if it's addition will
+            allow to compute another function
+
+            Args:
+                label_ty : `int` : One of the label types
+                v_from : `list` : List of parent vertex ids
+
+            Returns:
+                `int or None` : New vertex id or None if it's not contibuting
+                to the function set
+        """
+        neo = self._add_vertex(label_ty, v_from)
+
+        new_function = self.evaluate_vertex(neo)
+
+        if new_function not in self.collected_function_set:
+            self.collected_function_set.append(new_function)
+        else:
+            self._delete_vertex(neo)
+            return None
+
+        self.graph_evaluated = True
+        return neo
+
+
+    def inputs(self):
+        """
+            Returns list of ids of graph's inputs.
+        """
+        if self.reversed_linklist is None:
+            self.reversed_linklist = self.reversed_linklist
+        return [key for key, (label_ty, _) in self.reversed_linklist.items()
+                if label_ty == self.INPUT]
+
+
+    def evaluate_vertex(self, vtx):
+        """
+            Forwards values on graph to compute a functions in vtx.
+            Args:
+                vtx : `int` : Id of vertex in which to compute function.
+
+            Returns:
+                `list` : Function as a list with len = 2**self.n
+        """
+        new_function = []
+        for values in self.generate_inputs(length = self.n, ranges = [0, 1]):
+            evaluation = self._initiate_evaluation(values = values)
+            val = self._evaluate_vertex(v = vtx,
+                                        evaluation = evaluation)
+            new_function.append(val)
+        return new_function
+
+
+    def collect_functions(self):
+        """
+            Forwards the graph if needed and computes the reachable functions.
+            Returns:
+                `list` : list of functions
+        """
+        if self.graph_evaluated:
+            return self.collected_function_set
+
+        collected_functions = {v: [] for v in self.reversed_linklist}
+        for values in self.generate_inputs(self.n, ranges = [0, 1]):
+            evaluation = self._forward_graph(values)
+            for vtx in self.reversed_linklist:
+                collected_functions[vtx].append(evaluation[vtx])
+        self.graph_evaluated = True
+        self.collected_function_set = [func for vtx, func in collected_functions.items()]
+        return self.collected_function_set
+
+
+    def generate_inputs(self, length, ranges, _depth = 0):
+        """
+            Generator to produce increasing sequences
+            Args:
+                length : `int` : length of list
+                ranges : `list` : possible values
+
+            Yields:
+                `list` : next list of `length` elements in order defined by `ranges`
+        """
+        assert len(ranges) > 0
+        if _depth == length:
+            yield []
+            return
+        for value in ranges:
+            for called in self.generate_inputs(length, ranges, _depth + 1):
+                yield [value,] + called
+
 
     def _add_edge(self, v_from, v_to):
         self.reversed_linklist[v_to][1].append(v_from)
         self.graph_evaluated = False
 
-    def add_vertex(self, label_ty, v_from):
-        neo = self._add_vertex(label_ty, v_from)
-        
-        new_function = self.evaluate_vertex(neo)
-    
-        if new_function not in self.collected_function_set:
-            self.collected_function_set.append(new_function)
-        else:
-            self.delete_vertex(neo)
-            return None
-
-        self.graph_evaluated = True
-        return neo
 
     def _add_vertex(self, label_ty, v_from = []):
         new_index = len(self.reversed_linklist)
@@ -69,73 +156,37 @@ class PolarBear:
         return new_index
 
 
-    def delete_vertex(self, vtx):
+    def _delete_vertex(self, vtx):
         del self.reversed_linklist[vtx]
 
 
-    def evaluate_vertex(self, vtx):
-        new_function = []
-        # forward new vertex
-        for values in self.generate_inputs(length = self.n, ranges = [0, 1]):
-            val = self._evaluate_vertex(v = vtx, 
-                graph = self.reversed_linklist, 
-                evaluation = self.initiate_evaluation(graph = self.reversed_linklist, values = values))
-            new_function.append(val)
-        return new_function
-       
+    def _initiate_evaluation(self, values):
+        inputs = self.inputs()
+        assert len(inputs) == len(values), 'found {} inputs, not {}'.format(len(inputs), len(values))
 
-    def generate_inputs(self, length, ranges, _depth = 0):
-        assert len(ranges) > 0
-        if _depth == length:
-            yield []
-            return
-        for value in ranges:
-            for called in self.generate_inputs(length, ranges, _depth + 1):
-                yield [value,] + called
+        evaluation = {key: None for key in self.reversed_linklist}
+        for input_index in inputs:
+            evaluation[input_index] = values[input_index]
+        return evaluation
         
 
-    def _evaluate_vertex(self, v, graph, evaluation):
+    def _evaluate_vertex(self, v, evaluation):
         if evaluation[v] is not None:
             return evaluation[v]
-        func_ty, edges = graph[v]
+        func_ty, edges = self.reversed_linklist[v]
         collected = []
         for to in edges:
-            collected.append(self._evaluate_vertex(to, graph, evaluation))
-        #assert len(collected) == self.valences[func_ty], 'error in building, self.valences {} != {}'.format(len(collected), self.valences[func_ty])
+            collected.append(self._evaluate_vertex(to, evaluation))
         evaluation[v] = self.executive[func_ty](collected)
         return evaluation[v]
 
 
-    def initiate_evaluation(self, graph, values):
-        inputs = self.inputs(graph = graph)
-        assert len(inputs) == len(values), 'found {} inputs, not {}'.format(len(inputs), len(values))
-
-        evaluation = {key: None for key in graph}
-        for input_index in inputs:
-            evaluation[input_index] = values[input_index]
+    def _forward_graph(self, values):
+        evaluation = self._initiate_evaluation(values)
+        for key in self.reversed_linklist:
+            evaluation[key] = self._evaluate_vertex(key, evaluation)
         return evaluation
 
-    def _forward_graph(self, values, graph):
-        evaluation = self.initiate_evaluation(graph, values)
-        
-        for key in graph:
-            evaluation[key] = self._evaluate_vertex(key, graph, evaluation)
-        return evaluation
-        
-
-    def collect_functions(self):
-        if self.graph_evaluated:
-            return self.collected_function_set
-
-        collected_functions = {v: [] for v in self.reversed_linklist}
-        for values in self.generate_inputs(self.n, ranges = [0, 1]):
-            evaluation = self._forward_graph(values, graph = self.reversed_linklist)
-            for vtx in self.reversed_linklist:
-                collected_functions[vtx].append(evaluation[vtx])
-        self.graph_evaluated = True
-        self.collected_function_set = [func for vtx, func in collected_functions.items()]
-        self.collected_function_vtx = [vtx  for vtx, func in collected_functions.items()]
-        return self.collected_function_set
         
 def main():
 
@@ -172,31 +223,8 @@ def main():
             if neo is not None:
                 newest.append(neo)
 
-    '''
-    # pre-3. Find all functions with only one True 
-    bricks = [v for i, v in enumerate(g.collected_function_vtx) if g.collected_function_set[i].count(1) == 1]
-    # 3. Adding all other functions by disjuction
-    for f in sorted(g.generate_inputs(length = 8, ranges = [0, 1]), key = lambda item: item.count(1)):
-        g.add_vertex(g.OR, v_from = [b for i, b in enumerate(bricks) if f[i] == 1])
-    '''
-
-    '''for f in sorted(g.generate_inputs(length = 8, ranges = [0, 1]), key = lambda item: item.count(1)):
-                    for pos, value in enumerate(f):
-                        if value == 0:
-                            continue'''
-
-
-    
-    def report():
-        print ('Lost:')
-        diff = sorted([sq for sq in g.generate_inputs(length = 8, ranges = [0, 1]) if sq not in g.collect_functions()], key = lambda x: x.count(1))
-        pprint (diff)
-        print ('total {} functions lost'.format(len(diff)))    
-    #report()
-    
     return g
 
     
 if __name__ == '__main__':
-    g = main()
-    print (g)
+    print(main())
