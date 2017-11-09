d6cdfffdef280dd6ccd0c4176033ae35eea2553b
marqueewinq <marqueewinq@gmail.com>
2017-11-09 13:16:57 +0300

    WA 3 | det % MOD

--- task-2/main.py
+++ task-2/main.py
@@ -3,9 +3,35 @@ import numpy as np
 
 RANDINT_SEED = 1543
 RANDINT_HIGHER = 1000 * 1000 * 1000
+MOD = 1e9 + 7
 
-def make_graph(edges, ns, seed):
-	np.random.seed(seed)
+
+def det(matrix, mul = 1):
+	width = len(matrix)
+	if width == 1:
+		return mul * matrix[0][0]
+
+	sign = -1
+	answer = 0
+	for i in range(width):
+		m = []
+		for j in range(1, width):
+			minor = []
+			for k in range(width):
+				if k != i:
+					minor.append(matrix[j][k])
+			m.append(minor)
+
+		sign *= -1
+		answer += mul * solve(m, sign * matrix[0][i])
+
+	return answer % MOD
+
+#matrix = [[1,-2,3],[0,-3,-4],[0,0,-3]]
+#print(solve(matrix, 1))
+
+
+def make_graph(edges, ns):
 	n1, n2 = ns
 	mat = np.zeros(shape = (max(n1, n2), max(n1, n2)))
 	for a, b in edges:
@@ -15,11 +41,14 @@ def make_graph(edges, ns, seed):
 
 def find_pplc(edges, ns):
 	np.seterr(all = 'ignore')
+	np.random.seed(seed = RANDINT_SEED)
 	mat = None
 	iter_max = 10
 	for _ in range(iter_max):
-		mat = make_graph(edges, ns, seed = RANDINT_SEED)
+		mat = make_graph(edges, ns)
 		det = np.linalg.det(mat)
+		#print (det)
+		#print (mat)
 		if det != 0:
 			return True
 	return False
@@ -36,7 +65,7 @@ def main():
 			m_b = max(m_b, b)
 			edges.append([a, b])
 
-	if find_pplc(edges, [m_a + 1, m_b + 1]):
+	if n == 0 or find_pplc(edges, [m_a + 1, m_b + 1]):
 		print('yes')
 	else:
 		print('no')
