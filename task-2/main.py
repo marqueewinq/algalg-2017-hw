c8d14ad653b58af25a1ce2443d3a1b8880e765fe
marqueewinq <marqueewinq@gmail.com>
2017-11-09 12:27:11 +0300

    WA 3

---
+++ task-2/main.py
@@ -0,0 +1,46 @@
+import sys
+import numpy as np
+
+RANDINT_SEED = 1543
+RANDINT_HIGHER = 1000 * 1000 * 1000
+
+def make_graph(edges, ns, seed):
+	np.random.seed(seed)
+	n1, n2 = ns
+	mat = np.zeros(shape = (n1 + n2, n1 + n2,))
+	for a, b in edges:
+		mat[a, n1 + b] = np.random.randint(RANDINT_HIGHER)
+		mat[n1 + b, a] = np.random.randint(RANDINT_HIGHER)
+	return mat
+
+
+def find_pplc(edges, ns):
+	mat = None
+	iter_max = 10
+	for _ in range(iter_max):
+		mat = make_graph(edges, ns, seed = RANDINT_SEED)
+		det = np.linalg.det(mat)
+		if det != 0:
+			return True
+	return False
+
+
+def main():
+	edges = []
+	m_a = m_b = 0
+	with sys.stdin as f:
+		n = int(f.readline())
+		for _ in range(n):
+			a, b = list(map(int, f.readline().split()))
+			m_a = max(m_a, a)
+			m_b = max(m_b, b)
+			edges.append([a, b])
+
+	if find_pplc(edges, [m_a + 1, m_b + 1]):
+		print('yes')
+	else:
+		print('no')
+
+
+if __name__ == '__main__':
+	main()
